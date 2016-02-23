8a0e91d8d5659ec839c0e4cf4651912472af8531
Colin Carter <colin@codefashioned.com>
2016-02-23 16:05:24 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,54 @@
+# dmx
+
+dmx is a Yeoman generator to get you building interfaces faster. [Jekyll](http://jekyllrb.com/) is included for static site generation. [Bourbon](http://bourbon.io), [Neat](http://neat.bourbon.io) & [Bitters](http://bitters.bourbon.io/) are included to help you write CSS faster.
+
+[Grunt](http://gruntjs.com/) is used for compilation of [Sass](http://sass-lang.com) and [CoffeeScript](http://coffeescript.org) (optional). [Bower](http://bower.io/) is used for managing dependencies.
+
+### Prerequisites
+If you do not have [Node.js](http://nodejs.org/) `>=0.10`, [Yeoman](http://yeoman.io/) `>=1.1.2`, [Ruby](https://www.ruby-lang.org/en/) `>=1.9` and the [Bundler](http://bundler.io/) gem installed, you must do that first:
+
+- [Node.js](http://davidcalhoun.me/2013/12/16/developer-tools-homebrew/)
+- [Yeoman](http://yeoman.io/learning/index.html)
+- [Ruby](https://rvm.io/rvm/install)
+- [Bundler](http://bundler.io/#getting-started)
+
+### Installation
+````bash
+npm install -g generator-dmx
+````
+
+### Update
+
+If you already have generator-dmx installed, please upgrade before
+generating another site to get the latest updates.
+
+```bash
+npm list -g | grep 'generator-dmx' # See your installed version
+npm info generator-dmx | grep 'latest' # See latest generator-dmx version
+npm update -g generator-dmx # Upgrade generator-dmx globally
+```
+
+### Usage
+dmx will run `bundle install`, so if you would like to install the dmx gems into a gemset, set that up before running `yo dmx`.
+
+````bash
+mkdir project-name && cd project-name
+yo dmx
+````
+
+*Should you run into an error such as `command yo not found` it may be related to a path issue when installing Node.js via Homebrew. Please refer to the top answer on this [StackOverflow question](http://stackoverflow.com/questions/15846076/command-not-found-after-installation).*
+
+### Grunt Tasks
+##### grunt serve
+Serve your source locally into your browser. LiveReload will automatically load any changes to HTML, CSS and JavaScript that you make.
+
+##### grunt check
+Check the quality of your source with tools like [JSLint](http://www.jslint.com/), [CSSLint](http://csslint.net/) and [csscss](http://zmoazeni.github.io/csscss/).
+
+##### grunt build
+Build the concatenated, minified production version of the source into the `dist` directory.
+
+##### grunt deploy
+Deploy the production version of the source to [GitHub Pages](http://pages.github.com/). This ability is configurable during dmx generation.
+
+If you choose to utilize `grunt-build-control` to deploy a GitHub Pages user/organization site, your source must live in a branch other than `master`. GitHub Pages [user/organization sites](https://help.github.com/articles/user-organization-and-project-pages#project-pages) serve the files found in the `master` branch to the browser.
---
+++ app/index.js
@@ -0,0 +1,257 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var chalk = require('chalk');
+var yeoman = require('yeoman-generator');
+var globule = require('globule');
+var shelljs = require('shelljs');
+
+
+var dmxGenerator = module.exports = function dmxGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  var dependenciesInstalled = ['bundle', 'ruby'].every(function (depend) {
+    return shelljs.which(depend);
+  });
+
+  // Exit if Ruby dependencies aren't installed
+  if (!dependenciesInstalled) {
+    console.log('Looks like you\'re missing some dependencies.' +
+      '\nMake sure ' + chalk.white('Ruby') + ' and the ' + chalk.white('Bundler gem') + ' are installed, then run again.');
+    shelljs.exit(1);
+  }
+
+  this.gitInfo = {
+    name: this.user.git.username,
+    email: this.user.git.email
+  }
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.appName = path.basename(process.cwd());
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(dmxGenerator, yeoman.generators.Base);
+
+dmxGenerator.prototype.askForUser = function askForUser() {
+  var cb = this.async();
+  var prompts = [
+    {
+      name: 'authorName',
+      message: 'What is your name?',
+      default: this.gitInfo.name
+    },
+    {
+      name: 'authorEmail',
+      message: 'What is your email?',
+      default: this.gitInfo.email
+    }
+  ];
+
+  console.log(this.yeoman);
+  console.log(chalk.yellow('\nTell us a little about the project.') + ' →');
+
+  this.prompt(prompts, function (props) {
+
+    this.authorName  = props.authorName;
+    this.authorEmail = props.authorEmail;
+
+    cb();
+  }.bind(this));
+};
+
+dmxGenerator.prototype.askForTools = function askForTools() {
+  var cb = this.async();
+  var prompts = [
+    {
+      name: 'jsPre',
+      type: 'list',
+      message: 'JavaScript preproccesor',
+      choices: ['None', 'CoffeeScript']
+    },
+    {
+      name: 'sassComp',
+      type: 'list',
+      message: 'Sass compiler',
+      choices: ['Ruby', 'LibSass']
+    },
+    {
+      name: 'googleAnalytics',
+      type: 'confirm',
+      message: 'Include Google Analytics?',
+      default: false
+    }
+  ]
+
+  console.log(chalk.yellow('\nPreprocessors and tools.') + ' →');
+
+  this.prompt(prompts, function (props) {
+    this.googleAnalytics = props.googleAnalytics;
+
+    // Multiple choice 'None' to false
+    this.jsPre = props.jsPre === 'None' ? false : props.jsPre.toLowerCase();
+
+    // Lowercase sassComp variable
+    this.sassComp = props.sassComp.toLowerCase();
+
+    cb();
+  }.bind(this));
+};
+
+dmxGenerator.prototype.askForDeployment = function askForDeployment() {
+  var cb = this.async();
+  var prompts = [
+    {
+      name: 'deploy',
+      message: 'Use grunt-build-control for deployment?',
+      type: 'confirm',
+      default: false
+    },
+    {
+      name: 'deployHost',
+      type: 'list',
+      message: 'Host to deploy to',
+      choices: ['GitHub Pages', 'Generic remote'],
+      when: function (answers) {
+        return answers.deploy;
+      }
+    },
+    {
+      name: 'ghOwner',
+      message: 'GitHub repository owner',
+      when: function (answers) {
+        return answers.deployHost === 'GitHub Pages';
+      }
+    },
+    {
+      name: 'ghRepo',
+      message: 'GitHub repository name',
+      when: function (answers) {
+        return answers.deployHost === 'GitHub Pages';
+      }
+    },
+    {
+      name: 'ghPagesProject',
+      type: 'list',
+      message: 'GitHub Project or User/Organization site?',
+      choices: ['Project', 'User/Organization'],
+      when: function(answers) {
+        return answers.deployHost == 'GitHub Pages';
+      }
+    },
+    {
+      name: 'remoteURL',
+      message: 'Remote URL',
+      when: function (answers) {
+        return answers.deployHost === 'Generic remote';
+      }
+    },
+    {
+      name: 'deployBranch',
+      message: 'Branch to deploy to',
+      default: function(answers) {
+        if (answers.ghPagesProject === 'Project') {
+          return 'gh-pages';
+        } else if (answers.ghPagesProject === 'User/Organization') {
+          return 'master';
+        } else {
+          return 'dist';
+        }
+      },
+      when: function (answers) {
+        return answers.deploy;
+      }
+    }
+  ]
+
+  console.log(chalk.yellow('\nDeployment options.') + ' →');
+
+  this.prompt(prompts, function (props) {
+
+    this.deploy         = props.deploy;
+    this.deployHost     = props.deployHost;
+    this.ghOwner        = props.ghOwner;
+    this.ghRepo         = props.ghRepo;
+    this.deployBranch   = props.deployBranch;
+
+    if (props.ghPagesProject) {
+      this.ghPagesProject = props.ghPagesProject.replace('/', '_').toLowerCase();
+    }
+
+    if (this.deployHost === 'GitHub Pages') {
+      this.deployRemote = 'git@github.com:' + this.ghOwner + '/' + this.ghRepo + '.git';
+    } else {
+      this.deployRemote = props.remoteURL;
+    }
+
+    cb();
+  }.bind(this));
+};
+
+dmxGenerator.prototype.rubyDependencies = function rubyDependencies() {
+  this.template('Gemfile');
+  this.conflicter.resolve(function (err) {
+    if (err) {
+      return this.emit('error', err);
+    }
+    shelljs.exec('bundle install');
+  });
+};
+
+dmxGenerator.prototype.app = function app() {
+  this.directory('app', 'app');
+  this.copy('Gemfile', 'Gemfile');
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('csslintrc', '.csslintrc');
+  this.copy('gitignore', '.gitignore');
+  this.template('_Gruntfile.js', 'Gruntfile.js');
+  this.template('config.yml', '_config.yml');
+  this.template('_config.build.yml', '_config.build.yml');
+  this.template('_package.json', 'package.json');
+  this.template('_bower.json', 'bower.json');
+  this.template('_README.md', 'README.md');
+};
+
+dmxGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
+
+dmxGenerator.prototype.templates = function templates() {
+  this.template('conditional/template/default.html', 'app/_layouts/default.html');
+  this.template('conditional/template/index.html', 'app/index.html');
+
+  if (this.googleAnalytics) {
+    this.copy('conditional/template/google-analytics.html', 'app/_includes/shared/google-analytics.html');
+  };
+};
+
+dmxGenerator.prototype.jsPreprocessor = function jsPreprocessor() {
+  if (this.jsPre === 'coffeescript') {
+    this.copy('conditional/coffee/app.coffee', 'app/scripts/app.coffee');
+  } else {
+    this.copy('conditional/javascript/app.js', 'app/scripts/app.js');
+  }
+};
+
+dmxGenerator.prototype.installBitters = function installBitters() {
+  var root = shelljs.pwd();
+
+  // Install Bitters
+  shelljs.cd('app/styles');
+  shelljs.exec('bundle exec bitters install');
+  shelljs.cd(root);
+
+  // Replace Rails-style @import of neat-helpers
+  var gridSettings = shelljs.cat('app/styles/base/_grid-settings.scss');
+  gridSettings = gridSettings.replace(/^@import 'neat-helpers';.*/, "@import 'neat/app/assets/stylesheets/neat-helpers';");
+  gridSettings.to('app/styles/base/_grid-settings.scss');
+
+  // Uncomment Neat grid-settings @import
+  var base = shelljs.cat('app/styles/base/_base.scss');
+  base = base.replace(/^\/\/ @import 'grid-settings';.*/, "@import 'grid-settings';");
+  base.to('app/styles/base/_base.scss');
+};
